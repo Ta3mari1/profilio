c998899207d7dc0548d06a680c7ecbfaf09e7fec
Ahmad <ahmadalteneh1@gmail.com>
2025-01-06 22:25:50 -0600

    60% done with home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,93 @@
 import { Switch } from '../components/tailframes/Switch';
 import { Button } from '../components/tailframes/button';
+import profilePic from '../components/images/profilePic.jpg';
+import React, { useState, useEffect } from 'react';
+import '../pageStyle/Home.css';
 
 export default function Home() {
+
+    const [showPlaceholder, setShowPlaceholder] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPlaceholder(false);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+
+
+
+
     return (
         <section
             className="grid grid-flow-row flex-wrap items-center gap-x-0 gap-y-6 py-6 text-center md:gap-x-28 md:py-16 lg:grid-cols-2 lg:text-start max-w-screen-2xl m-auto w-full px-3 sm:px-8 lg:px-16 xl:px-32"
         >
-            <div
-                className="flex w-full max-w-2xl items-center justify-center justify-self-center rounded-2xl bg-slate-50 py-20 md:py-32 lg:justify-self-end"
-            >
+            <div className="w-full h-1/8 flex items-center justify-center bg-slate-50 rounded-2xl">
                 <img
-                    src="https://www.tailframes.com/images/illustration.webp"
-                    alt=""
-                    width={183}
-                    height={345}
-                    className="h-[172px] w-[91px] md:h-[345px] md:w-[183px]"
+                    src={profilePic}
+                    alt="Profile Picture"
+                    className="w-full h-full object-cover object-top"
                 />
             </div>
-            <div className="flex flex-col gap-6 justify-self-center md:gap-12 lg:justify-self-start">
-                <div className="flex max-w-lg flex-col gap-6">
-                    <p className="text-sm font-semibold uppercase text-slate-500">
-                        Tailframes
-                    </p>
-                    <h3 className="text-4xl font-semibold text-slate-950 md:text-5xl">
-                        Copy & Paste.
-                        <br />
-                        It's that simple.
-                    </h3>
-                    <p className="text-lg font-normal tracking-tight text-slate-500">
-                        We've done it carefully and simply. The elements work well together to create stunning landings.
-                    </p>
+
+            {showPlaceholder ? (
+
+                <div className="flex flex-col items-center justify-center mx-auto max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl  text-white p-5 rounded-lg shadow-lg overflow-hidden">
+                    <div className='magic text-center text-lg font-bold mb-3'>Hello there </div>
+                    <div className='text-xs sm:text-sm md:text-base overflow-x-auto whitespace-pre-wrap  ' >
+                        ⢀⣠⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⡾⠿⠿⠿⠿⢷⣶⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⢰⣿⡟⠛⠛⠛⠻⠿⠿⢿⣶⣶⣦⣤⣤⣀⣀⡀⣀⣴⣾⡿⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠻⢿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⡀
+                        ⠀⠻⣿⣦⡀⠀⠉⠓⠶⢦⣄⣀⠉⠉⠛⠛⠻⠿⠟⠋⠁⠀⠀⠀⣤⡀⠀⠀⢠⠀⠀⠀⣠⠀⠀⠀⠀⠈⠙⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠛⢻⣿
+                        ⠀⠀⠈⠻⣿⣦⠀⠀⠀⠀⠈⠙⠻⢷⣶⣤⡀⠀⠀⠀⠀⢀⣀⡀⠀⠙⢷⡀⠸⡇⠀⣰⠇⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⡶⠶⠶⠒⠂⠀⠀⣠⣾⠟
+                        ⠀⠀⠀⠀⠈⢿⣷⡀⠀⠀⠀⠀⠀⠀⠈⢻⣿⡄⣠⣴⣿⣯⣭⣽⣷⣆⠀⠁⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣦⡀⠀⣠⣾⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⣠⣾⡟⠁⠀
+                        ⠀⠀⠀⠀⠀⠈⢻⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿⡗⢻⣿⣧⣽⣿⣿⣿⣧⠀⠀⣀⣀⠀⢠⣿⣧⣼⣿⣿⣿⣿⠗⠰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠋⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠙⢿⣶⣄⡀⠀⠀⠀⠀⠸⠃⠈⠻⣿⣿⣿⣿⣿⡿⠃⠾⣥⡬⠗⠸⣿⣿⣿⣿⣿⡿⠛⠀⢀⡟⠀⠀⠀⠀⠀⠀⣀⣠⣾⡿⠋⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣷⣶⣤⣤⣄⣰⣄⠀⠀⠉⠉⠉⠁⠀⢀⣀⣠⣄⣀⡀⠀⠉⠉⠉⠀⠀⢀⣠⣾⣥⣤⣤⣤⣶⣶⡿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢻⣿⠛⢿⣷⣦⣤⣴⣶⣶⣦⣤⣤⣤⣤⣬⣥⡴⠶⠾⠿⠿⠿⠿⠛⢛⣿⣿⣿⣯⡉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣧⡀⠈⠉⠀⠈⠁⣾⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⠟⠉⣹⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣿⣿⣦⣀⠀⠀⠀⢻⡀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣶⣿⠋⣿⠛⠃⠀⣈⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡿⢿⡀⠈⢹⡿⠶⣶⣼⡇⠀⢀⣀⣀⣤⣴⣾⠟⠋⣡⣿⡟⠀⢻⣶⠶⣿⣿⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣷⡈⢿⣦⣸⠇⢀⡿⠿⠿⡿⠿⠿⣿⠛⠋⠁⠀⣴⠟⣿⣧⡀⠈⢁⣰⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⢻⣦⣈⣽⣀⣾⠃⠀⢸⡇⠀⢸⡇⠀⢀⣠⡾⠋⢰⣿⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⢿⣿⣿⡟⠛⠃⠀⠀⣾⠀⠀⢸⡇⠐⠿⠋⠀⠀⣿⢻⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠁⢀⡴⠋⠀⣿⠀⠀⢸⠇⠀⠀⠀⠀⠀⠁⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠟⠋⠀⠀⠀⣿⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣁⣀⠀⠀⠀⠀⣿⡀⠀⣿⠀⠀⠀⠀⠀⠀⢀⣈⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</div>
                 </div>
-                <div>
-                    <Button size="large">
-                        Get a Template
-                    </Button>
+
+
+
+
+            ) : (
+                <div className="flex flex-col gap-6 justify-self-center md:gap-12 lg:justify-self-start">
+                    <div className="flex max-w-lg flex-col gap-6">
+                        <p className="text-sm font-semibold uppercase text-slate-500">
+                            👋 Welcome to My Portfolio!
+                        </p>
+                        <h3 className="text-4xl font-semibold text-slate-950 md:text-4xl">
+                            Hey, I’m Ahmad Alteneh, a Computer Science student with a passion for Software Devolpment, algorithms, and problem-solving. I love building clean, efficient, and scalable applications.
+                            <br />
+
+                        </h3>
+                        <p className="text-lg font-normal tracking-tight text-slate-500">
+                            ✨ Enough about me—feel free to explore the other sections to learn more! 🚀
+                        </p>
+                    </div>
+
                 </div>
-            </div>
+
+            )
+
+            }
+
+
+
+
+
+
         </section>
-    )
+    );
 }
