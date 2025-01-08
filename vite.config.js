345ad2d18af6f983149719ee2530c2157ac740e9
Ahmad <ahmadalteneh1@gmail.com>
2025-01-07 21:17:50 -0600

    Fixed base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/profolio/',
+  base: '/profilio/',
   server: {
     hmr: {
       overlay: false, // Prevents error overlay from blocking UI
